467371887085c67443e7a0fcdf32745b5137f92c
lucasGames-cityboy <lucas.oliveira.lisboa@escola.pr.gov.br>
2026-03-10 11:39:44 -0300

    Add files via upload

---
+++ endereco.js
@@ -0,0 +1,8 @@
+console.log("Endereço completo");
+let rua = "Rua das flores";
+let numero = 33;
+let cidade = "Cityboy";
+
+let Endereco = rua + ", " + numero  + " - " + cidade;
+//console.log (Endereco);
+console.log("O endereço correto é: " + Endereco);
