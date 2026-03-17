7b28ada69d84b3a5e57608f9207aeaef4ea33683
lucasGames-cityboy <lucas.oliveira.lisboa@escola.pr.gov.br>
2026-03-17 14:39:41 +0000

    versao2

--- hora.js
+++ hora.js
@@ -1,3 +1,10 @@
 //Valores iniciais do dia
 let valorPorHora = 50;
 let HorasTrabalhadas = 8;
+
+console.log("---Resumo do Expediente---")
+console.log(`Horas iniciais: ${HorasTrabalhadas}h.`);
+
+//git add hora.js
+//git commit -m "vesao2"
+//git pusk
