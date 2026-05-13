f3094224483a6da94245e08849b8d131b3f9363a
lucasGames-cityboy <lucas.oliveira.lisboa@escola.pr.gov.br>
2026-05-13 13:53:41 +0000

    commit

---
+++ desafio.js
@@ -0,0 +1,15 @@
+//declarar 5 variaveis para representar o perfil de um jogador
+//fazer uma saida de forma interpolada (`!${}`)
+//jogador basico? idade? apelido? jogo? time?
+
+let nomeJogador = "joão";
+let apelido = "joãozinho";
+let idade = "19";
+let jogo = "futebol";
+let time = "grêmio";
+console.log(`---Perfil do jogador---
+    nome: ${nomeJogador} 
+    apelido: ${apelido}
+    idade: ${idade}
+    jogo: ${jogo}
+    time: ${time}`)
